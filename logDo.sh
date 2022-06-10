e4dbd578b1ef5e9d8f1926156314904712b25fa1
28HuaSheng <46739345+28-HuaSheng@users.noreply.github.com>
2022-06-10 16:18:39 +0800

    Update logDo.sh

--- logDo.sh
+++ logDo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+# 单文件匹配
 cat info.log.2022-06-01.log | grep '********' | head -10 | awk -F 'campaign_id=' '{print $2}' | awk -F '&' '{print $1}'
+# 多文件匹配
+cat info.log.2022-06*.log | grep 'api1/|api2' | grep 'A\|B\|C' | wc -l
