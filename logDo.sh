fe3f55cdf707b3b07e405d319d7d72ac02c42eff
28HuaSheng <46739345+28-HuaSheng@users.noreply.github.com>
2022-06-24 10:56:49 +0800

    Update logDo.sh

--- logDo.sh
+++ logDo.sh
@@ -5,5 +5,6 @@
 当前目录排序  du -s * | sort -nr | head
 # 单文件匹配
 cat info.log.2022-06-01.log | grep '********' | head -10 | awk -F 'campaign_id=' '{print $2}' | awk -F '&' '{print $1}'
+cat info.log.2022-06-15.log | grep 'K_userName' | awk -F 'K_userName = ' '{print $2}' | awk -F ']' '{print $1}' | sort | uniq >> tmp.username.out
 # 多文件匹配
 cat info.log.2022-06*.log | grep 'api1/|api2' | grep 'A\|B\|C' | wc -l
